6fd1d4f32b3540006a3f3d37567db1beecb0e113
Junjie Ding <djjoriens@gmail.com>
2020-04-05 12:19:53 +0800

    update

--- update.code.sh
+++ update.code.sh
@@ -1,5 +1,5 @@
 git add .
 git commit -m "update"
-git push git@github.com:IOriens/mina-wiki.git dev
-git push https://git.dev.tencent.com/IOriens/mina-wiki.git dev
-git push https://git.dev.tencent.com/IOriens/mina.git dev
+git push git@github.com:IOriens/mina-wiki.git master:dev
+git push https://git.dev.tencent.com/IOriens/mina-wiki.git master:dev
+git push https://git.dev.tencent.com/IOriens/mina.git master:dev
