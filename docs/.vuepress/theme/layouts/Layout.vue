6b833bde7f8de6b54a2098f9b2c0bf77e6950703
Junjie Ding <djjoriens@gmail.com>
2020-10-19 00:14:32 +0800

    update

---
+++ docs/.vuepress/theme/layouts/Layout.vue
@@ -0,0 +1,27 @@
+<style scoped>
+.custom-footer {
+  text-align: center;
+  font-size: 16px;
+  font-weight: 500;
+}
+</style>
+
+<template>
+  <ParentLayout>
+    <template #page-bottom>
+      <div class="custom-footer">✨ 进小程序开发者交流群请加我微信: <code>jjjdev</code> ✨</div>
+    </template>
+  </ParentLayout>
+</template>
+
+<script>
+import ParentLayout from '@parent-theme/layouts/Layout.vue'
+
+export default {
+  name: 'Layout',
+
+  components: {
+    ParentLayout
+  }
+}
+</script>
